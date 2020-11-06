7263c1074cbc1a3ac8368db031e20099c1ef969a
christian-fuss <christian-fuss@users.noreply.github.com>
2020-11-06 17:29:41 +0000

    deploy: 9deb96c3acf0947ae08dc6073bc7c2695124242c

--- _next/static/yJttGXcC9xTkb9YAxJ-pd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ba3869ff83aa72272a1c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d8f21d779e9463d1a83e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
